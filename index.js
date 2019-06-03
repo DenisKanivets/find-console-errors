ac769f305576a80ef4b41b3e055b53b9c92771d8
Denis Kanivets <denis.kanivets@ukr.net>
2019-06-03 18:29:15 +0300

    version-1.1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 const xl = require('excel4node');
 
 (async () => {
-    //enter username and password in string type
+    //enter username and password here
     let username = 'TestConsole';
     let password = 'TestConsole';
 
@@ -31,46 +31,41 @@ const xl = require('excel4node');
         'limit': 999,
         'parameterValues': [{"key": "@UserName", "value": username}]
     };
+    let dashboardArr = [];
     let urlsArr = [];
     await axios.post('https://dashboard.kyivcity.gov.ua/api/ds/utility/query/getvalues', sendObjForUrls, {headers: headersForUrls})
-        .then(res => urlsArr = res.data)
+        .then(res => dashboardArr = res.data)
         .catch(err => console.log(err));
-    urlsArr = urlsArr.rows.map(item => item.values[0]);
-    console.log(urlsArr);
-    console.log('quantity of dashboards: ' + urlsArr.length);
+    dashboardArr = dashboardArr.rows.map(item => item.values[0]);
+    console.log(dashboardArr);
+    console.log('quantity of dashboards: ' + dashboardArr.length);
     console.log('====================URLS REQUEST FINISHED====================');
 
     //check for errors
     const browser = await puppeteer.launch();
-    const headers = {
-        'Authorization': 'Bearer ' + bearer
-    };
+    const headers = {'Authorization': 'Bearer ' + bearer};
     let errArr = [];
     let timeArr = [];
-    for (let i = 0; i < urlsArr.length; i++) {
+    let splice = [];
+    for (let i = 0; i < dashboardArr.length; i++) {
         let oldTime = new Date();
-        const url = 'https://dashboard.kyivcity.gov.ua/dashboard/page/' + urlsArr[i];
+        urlsArr.push('https://dashboard.kyivcity.gov.ua/dashboard/page/' + dashboardArr[i]);
+        const url = urlsArr[i];
         console.log(url);
         const page = await browser.newPage();
         page.setExtraHTTPHeaders(headers);
-        let check = false;
         page.on('console', msg => {
-            check = true;
             msg._args.map(item => {
                 if (item._remoteObject.type === 'object' && item._remoteObject.subtype === 'error') {
                     let obj = {};
                     obj[url] = item._remoteObject.description;
+                    splice.push(i);
                     errArr.push(obj);
                     console.log('error was founded')
                 }
             })
         });
-        if (!check) {
-            let obj = {};
-            obj[url] = 'NO ERRORS';
-            errArr.push(obj);
-            console.log('no error')
-        }
+        page.setDefaultTimeout(0);
         await page.goto(url, {waitUntil: 'networkidle0'});
         let newTime = new Date();
         let spentTime = newTime - oldTime;
@@ -79,9 +74,23 @@ const xl = require('excel4node');
     await browser.close();
     console.log('====================CHECKING FOR ERRORS FINISHED====================');
 
+    //sort errors array
+    function onlyUnique(value, index, self) {
+        return self.indexOf(value) === index;
+    }
+    let spliceAll = splice.filter(onlyUnique);
+    for (let s = 0; s < spliceAll.length; s++) {
+        urlsArr.splice(spliceAll[s] - s, 1);
+    }
+    for (let j = 0; j < urlsArr.length; j++) {
+        let obj = {};
+        obj[urlsArr[j]] = 'no errors';
+        errArr.push(obj);
+    }
+
     //write errors to file
     let wb = new xl.Workbook();
-    let ws = wb.addWorksheet('Sheet with errors');
+    let ws = wb.addWorksheet('Errors');
     for (let i = 0; i < errArr.length; i++) {
         for (let key in errArr[i]) {
             ws.cell(i + 1, 1)
@@ -90,10 +99,17 @@ const xl = require('excel4node');
                 .string(errArr[i][key])
                 .style({alignment: {wrapText: true}});
         }
-        ws.cell(i + 1, 3)
-            .string(timeArr[i] / 1000 + ' sec');
         ws.column(1).setWidth(75);
         ws.column(2).setWidth(100);
+    }
+    let ws2 = wb.addWorksheet('Load Time');
+    for (let z = 0; z < dashboardArr.length; z++) {
+        ws2.cell(z + 1, 1)
+            .string(dashboardArr[z]);
+        ws2.cell(z + 1, 2)
+            .string(timeArr[z] / 1000 + ' sec')
+            .style({alignment: {wrapText: true}});
+        ws2.column(1).setWidth(75);
     }
     wb.write(`errors_by_user_(${username}).xlsx`);
     console.log('====================NEW FILE CREATED====================');
